53eb4c87d83aa4f25abad2f8f47405a10cbec578
Josh Parker <joshBBparker@gmail.com>
2022-03-15 14:32:45 -0600

    marketing landing prop types

--- src/views/marketing/MarketingLanding/MarketingLanding.jsx
+++ src/views/marketing/MarketingLanding/MarketingLanding.jsx
@@ -1,34 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import Carousel from '../../../components/Carousel/Carousel';
 import LowerQuote from '../../../components/Carousel/LowerQuote';
 import UpperText from '../../../components/Carousel/UpperText';
 import GetStartedBtn from '../../../components/GetStartedBtn/GetStartedBtn';
 
-class MarketingLanding extends Component {
-  state = {
-    currentIndex: 0,
-  };
+const MarketingLanding = function MarketingLanding({ toggle }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  handleChange = (i) => this.setState({ currentIndex: i });
-
-  render() {
-    return (
-      <div className="market-land-container">
-        <div className="market-top-container">
-          <UpperText currentIndex={this.state.currentIndex} />
-        </div>
-        <Carousel handleChange={this.handleChange} />
-        <LowerQuote currentIndex={this.state.currentIndex} />
-        <div className="workflow-container">
-          <div className="workflow-text">
-            <span><h1>Workflow made simple.</h1></span>
-            <span style={{ color: 'blue' }}><h1>Try it for free.</h1></span>
-          </div>
-          <GetStartedBtn changeNav={this.props.toggle} />
-        </div>
+  return (
+    <div className="market-land-container">
+      <div className="market-top-container">
+        <UpperText currentIndex={currentIndex} />
       </div>
-    );
-  }
-}
+      <Carousel handleChange={setCurrentIndex} />
+      <LowerQuote currentIndex={currentIndex} />
+      <div className="workflow-container">
+        <div className="workflow-text">
+          <span><h1>Workflow made simple.</h1></span>
+          <span style={{ color: 'blue' }}><h1>Try it for free.</h1></span>
+        </div>
+        <GetStartedBtn changeNav={toggle} />
+      </div>
+    </div>
+  );
+};
+
+MarketingLanding.propTypes = {
+  toggle: PropTypes.bool.isRequired,
+};
 
 export default MarketingLanding;
