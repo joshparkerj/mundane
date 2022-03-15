32e9f0966e7996cf7261424f0f5799ab51c02736
Josh Parker <joshBBparker@gmail.com>
2022-03-15 15:28:48 -0600

    lint top nav bar

--- src/components/TopNavBar/TopNavBar.jsx
+++ src/components/TopNavBar/TopNavBar.jsx
@@ -1,104 +1,113 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
 import { logout } from '../../redux/actions';
 import './TopNavBar.scss';
 import NewTeamButton from '../NewTeam/NewTeamButton';
 
-class TopNavBar extends Component {
-  state = {
-    profileHidden: false,
+const TopNavBar = function TopNavBar({ page, user, logout: logoutFunc }) {
+  const [profileHidden, setProfileHidden] = useState(false);
+  const [searchInputValue, setSearchInputValue] = useState('');
+
+  const handleClick = () => {
+    setProfileHidden(!profileHidden);
   };
 
-  handleClick = () => {
-    this.setState({ profileHidden: !this.state.profileHidden });
+  const handleAdmin = () => {
+    page('/dashboard/admin');
   };
 
-  handleAdmin = () => {
-    this.props.page('/dashboard/admin');
+  const handleProfile = () => {
+    page('/dashboard/profile');
   };
 
-  handleProfile = () => {
-    this.props.page('/dashboard/profile');
-  };
-
-  searchSite = ({ key, target, target: { value } }) => {
+  const searchSite = ({ key }) => {
     if (key === 'Enter') {
-      target.value = '';
+      setSearchInputValue('');
     }
   };
 
-  render() {
-    return (
-      <div className="navbar-wrapper">
-        <div className="logo-tab-wrapper">
-          <div className="logo-background">
-            <div className="dash-nav-logo" />
-          </div>
-          <div className="first-three">
-            <div className="navbar-tab">
-              <span className="text">
-                <div className="add-member-span">
-                  <div className="dropdown-notifs">
-                    <i className="material-icons">notifications_none</i>
-                    <div className="dropdown-content">
-                      Notifications
-                    </div>
-                  </div>
-                </div>
-              </span>
-            </div>
-            <div className="navbar-tab">
-              <span className="text">
-                <div className="dropdown-people">
-                  <i className="material-icons">people_outline</i>
-                  <div className="dropdown-button">
-                    <NewTeamButton />
-                  </div>
-                </div>
-              </span>
-            </div>
-            <div className="navbar-tab">
-              <div className="container">
-                <input
-                  type="text"
-                  placeholder="Search our site."
-                  onKeyDown={this.searchSite}
-                />
-                <div className="search" />
-              </div>
-            </div>
-          </div>
+  return (
+    <div className="navbar-wrapper">
+      <div className="logo-tab-wrapper">
+        <div className="logo-background">
+          <div className="dash-nav-logo" />
         </div>
-        <div className="last-two">
+        <div className="first-three">
           <div className="navbar-tab">
             <span className="text">
               <div className="add-member-span">
-                <i className="material-icons">add_circle_outline</i>
-                <p> Invite Team Members </p>
+                <div className="dropdown-notifs">
+                  <i className="material-icons">notifications_none</i>
+                  <div className="dropdown-content">
+                    Notifications
+                  </div>
+                </div>
               </div>
             </span>
           </div>
           <div className="navbar-tab">
-            <div onClick={this.handleClick} className={`add-member-span ${this.state.profileHidden ? 'active' : ''}`}>
-              <img src={this.props.user.pic} className="topNav-user-pic" alt="" />
-              <p className="user-profile-menu"> UserProfile</p>
-              {this.state.profileHidden
-                ? (
-                  <div>
-                    <ul className="user-profile-menu-list">
-                      <li onClick={this.handleProfile}>Profile</li>
-                      <li onClick={this.handleAdmin}>Admin </li>
-                      <li>Recycle Bin </li>
-                      <li onClick={this.props.logout}>Logout</li>
-                    </ul>
-                  </div>
-                ) : (null)}
+            <span className="text">
+              <div className="dropdown-people">
+                <i className="material-icons">people_outline</i>
+                <div className="dropdown-button">
+                  <NewTeamButton />
+                </div>
+              </div>
+            </span>
+          </div>
+          <div className="navbar-tab">
+            <div className="container">
+              <input
+                value={searchInputValue}
+                onChange={({ target }) => setSearchInputValue(target.value)}
+                type="text"
+                placeholder="Search our site."
+                onKeyDown={searchSite}
+              />
+              <div className="search" />
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+      <div className="last-two">
+        <div className="navbar-tab">
+          <span className="text">
+            <div className="add-member-span">
+              <i className="material-icons">add_circle_outline</i>
+              <p> Invite Team Members </p>
+            </div>
+          </span>
+        </div>
+        <div className="navbar-tab">
+          <div tabIndex="-1" role="switch" aria-checked={profileHidden} onClick={handleClick} onKeyPress={handleClick} className={`add-member-span ${profileHidden ? 'active' : ''}`}>
+            <img src={user.pic} className="topNav-user-pic" alt="" />
+            <p className="user-profile-menu"> UserProfile</p>
+            {profileHidden
+              ? (
+                <div>
+                  <ul className="user-profile-menu-list">
+                    <li><button type="button" onClick={handleProfile}>Profile</button></li>
+                    <li><button type="button" onClick={handleAdmin}>Admin </button></li>
+                    <li>Recycle Bin </li>
+                    <li><button type="button" onClick={logoutFunc}>Logout</button></li>
+                  </ul>
+                </div>
+              ) : (null)}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+TopNavBar.propTypes = {
+  page: PropTypes.func.isRequired,
+  user: PropTypes.shape({
+    pic: PropTypes.string.isRequired,
+  }).isRequired,
+  logout: PropTypes.func.isRequired,
+};
 
 export default connect(null, { logout })(TopNavBar);
