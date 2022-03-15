bbbe0632ecf66c1835c44ce9e7bbd4698618b73d
Josh Parker <joshBBparker@gmail.com>
2022-03-15 17:05:28 -0600

    lint personal

--- src/components/MyProfile/components/Personal.jsx
+++ src/components/MyProfile/components/Personal.jsx
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Personal.scss';
 import Popup from 'reactjs-popup';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { setPhone, setTitle, setEmail } from '../../../redux/actions';
 
 const contentStyle = {
@@ -9,129 +10,156 @@ const contentStyle = {
   width: '90%',
 };
 
-class Personal extends Component {
-  state = {
-    email: '',
-    phone: '',
-    title: '',
-  };
+const Personal = function Personal({
+  user, setPhone: setDispatchPhone, setTitle: setDispatchTitle, setEmail: setDispatchEmail,
+}) {
+  const [stateEmail, setStateEmail] = useState('');
+  const [statePhone, setStatePhone] = useState('');
+  const [stateTitle, setStateTitle] = useState('');
 
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
-  };
-
-  handleClick = (close, propsMethod, name) => {
-    this.props[propsMethod]({ [name]: this.state[name] });
-    close();
-  };
-
-  render() {
-    return (
-      <div className="personal_info_container">
-        <h2>Overview</h2>
-        <div className="profile_edit_container">
-          <div className="icon_container">
-            <i className="far fa-user" />
-          </div>
-          <Popup
-            trigger={(
-              <div className="data_container_popup_edit_link">
-                <span className="edit_text_title">
-                  Title:
-                  {this.props.user.title}
-                </span>
-                <i className="fas fa-pencil-alt" />
-              </div>
-)}
-            modal
-            contentStyle={contentStyle}
-          >
-            {(close) => (
-              <div className="edit_form">
-                <div className="close" onClick={close}>&times;</div>
-                <div className="header"> Title</div>
-                <br />
-                <div className="ui input">
-                  <input type="text" id="title_input" name="title" value={this.state.title} onChange={this.handleChange} />
-                </div>
-                <br />
-                <div className="save-title-btn">
-                  <button className="save" onClick={() => this.handleClick(close, 'setTitle', 'title')}>Save</button>
-                </div>
-              </div>
-            )}
-          </Popup>
+  return (
+    <div className="personal_info_container">
+      <h2>Overview</h2>
+      <div className="profile_edit_container">
+        <div className="icon_container">
+          <i className="far fa-user" />
         </div>
-        <div className="profile_edit_container">
-          <div className="icon_container">
-            <i className="fas fa-envelope-square" />
-          </div>
-          <Popup
-            trigger={(
-              <div className="data_container_popup_edit_link">
-                <span className="edit_text_title">
-                  Email:
-                  {this.props.user.email}
-                </span>
-                <span className="profile-field-content" />
-                <i className="fas fa-pencil-alt" />
+        <Popup
+          trigger={(
+            <div className="data_container_popup_edit_link">
+              <span className="edit_text_title">
+                Title:
+                {user.title}
+              </span>
+              <i className="fas fa-pencil-alt" />
+            </div>
+          )}
+          modal
+          contentStyle={contentStyle}
+        >
+          {(close) => (
+            <div className="edit_form">
+              <button type="button" className="close" onClick={close}>&times;</button>
+              <div className="header"> Title</div>
+              <br />
+              <div className="ui input">
+                <input type="text" id="title_input" name="title" value={stateTitle} onChange={({ target }) => setStateTitle(target.value)} />
               </div>
-)}
-            modal
-            contentStyle={contentStyle}
-          >
-            {(close) => (
-              <div className="edit_form">
-                <div className="close" onClick={close}>&times;</div>
-                <div className="header"> Email</div>
-                <br />
-                <div className="ui input">
-                  <input type="text" id="title_input" name="email" value={this.state.email} onChange={this.handleChange} />
-                </div>
-                <br />
-                <div className="save-title-btn">
-                  <button className="save" onClick={() => this.handleClick(close, 'setEmail', 'email')}>Save</button>
-                </div>
+              <br />
+              <div className="save-title-btn">
+                <button
+                  type="submit"
+                  className="save"
+                  onClick={() => {
+                    setDispatchTitle(stateTitle);
+                    close();
+                  }}
+                >
+                  Save
+                </button>
               </div>
-            )}
-          </Popup>
-        </div>
-        <div className="profile_edit_container">
-          <div className="icon_container">
-            <i className="fas fa-phone" />
-          </div>
-          <Popup
-            trigger={(
-              <div className="data_container_popup_edit_link">
-                <span className="edit_text_title">
-                  Phone:
-                  {this.props.user.phone}
-                </span>
-                <i className="fas fa-pencil-alt" />
-              </div>
-)}
-            modal
-            contentStyle={contentStyle}
-          >
-            {(close) => (
-              <div className="edit_form">
-                <div className="close" onClick={close}>&times;</div>
-                <div className="header"> Phone</div>
-                <br />
-                <div className="ui input">
-                  <input type="text" id="title_input" name="phone" value={this.state.phone} onChange={this.handleChange} />
-                </div>
-                <br />
-                <div className="save-title-btn">
-                  <button className="save" onClick={() => this.handleClick(close, 'setPhone', 'phone')}>Save</button>
-                </div>
-              </div>
-            )}
-          </Popup>
-        </div>
+            </div>
+          )}
+        </Popup>
       </div>
-    );
-  }
-}
+      <div className="profile_edit_container">
+        <div className="icon_container">
+          <i className="fas fa-envelope-square" />
+        </div>
+        <Popup
+          trigger={(
+            <div className="data_container_popup_edit_link">
+              <span className="edit_text_title">
+                Email:
+                {user.email}
+              </span>
+              <span className="profile-field-content" />
+              <i className="fas fa-pencil-alt" />
+            </div>
+          )}
+          modal
+          contentStyle={contentStyle}
+        >
+          {(close) => (
+            <div className="edit_form">
+              <button type="button" className="close" onClick={close}>&times;</button>
+              <div className="header"> Email</div>
+              <br />
+              <div className="ui input">
+                <input type="text" id="title_input" name="email" value={stateEmail} onChange={({ target }) => setStateEmail(target.value)} />
+              </div>
+              <br />
+              <div className="save-title-btn">
+                <button
+                  type="submit"
+                  className="save"
+                  onClick={() => {
+                    setDispatchEmail(stateEmail);
+                    close();
+                  }}
+                >
+                  Save
+                </button>
+              </div>
+            </div>
+          )}
+        </Popup>
+      </div>
+      <div className="profile_edit_container">
+        <div className="icon_container">
+          <i className="fas fa-phone" />
+        </div>
+        <Popup
+          trigger={(
+            <div className="data_container_popup_edit_link">
+              <span className="edit_text_title">
+                Phone:
+                {user.phone}
+              </span>
+              <i className="fas fa-pencil-alt" />
+            </div>
+          )}
+          modal
+          contentStyle={contentStyle}
+        >
+          {(close) => (
+            <div className="edit_form">
+              <button type="button" className="close" onClick={close}>&times;</button>
+              <div className="header"> Phone</div>
+              <br />
+              <div className="ui input">
+                <input type="text" id="title_input" name="phone" value={statePhone} onChange={({ target }) => setStatePhone(target.value)} />
+              </div>
+              <br />
+              <div className="save-title-btn">
+                <button
+                  type="submit"
+                  className="save"
+                  onClick={() => {
+                    setDispatchPhone(statePhone);
+                    close();
+                  }}
+                >
+                  Save
+                </button>
+              </div>
+            </div>
+          )}
+        </Popup>
+      </div>
+    </div>
+  );
+};
+
+Personal.propTypes = {
+  user: PropTypes.shape({
+    phone: PropTypes.string,
+    email: PropTypes.string,
+    title: PropTypes.string,
+  }).isRequired,
+  setPhone: PropTypes.func.isRequired,
+  setEmail: PropTypes.func.isRequired,
+  setTitle: PropTypes.func.isRequired,
+};
 
 export default connect(null, { setPhone, setEmail, setTitle })(Personal);
