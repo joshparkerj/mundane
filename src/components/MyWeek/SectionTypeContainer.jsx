a9e262837377de221846f49d29b36b22685d462d
Josh Parker <joshBBparker@gmail.com>
2022-03-15 15:36:22 -0600

    lint section type container

--- src/components/MyWeek/SectionTypeContainer.jsx
+++ src/components/MyWeek/SectionTypeContainer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import DropDownWeek from '../DropDownWeek/DropDownWeek';
 
 function SectionTypeContainer({
@@ -6,11 +8,19 @@ function SectionTypeContainer({
 }) {
   return (
     <div className="section-type-container">
-      <span onClick={hc}>{bt}</span>
+      <span tabIndex="-1" role="link" onKeyPress={hc} onClick={hc}>{bt}</span>
       <span className="assignmentCounter">{ac}</span>
       {sdd ? (<DropDownWeek nav={nav} />) : (null)}
     </div>
   );
 }
+
+SectionTypeContainer.propTypes = {
+  hc: PropTypes.func.isRequired,
+  bt: PropTypes.string.isRequired,
+  ac: PropTypes.string.isRequired,
+  nav: PropTypes.string.isRequired,
+  sdd: PropTypes.bool.isRequired,
+};
 
 export default SectionTypeContainer;
