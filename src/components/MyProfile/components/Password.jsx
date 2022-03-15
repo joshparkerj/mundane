47485fa0b4b3e04c0b47b38ed0ef54a476419134
Josh Parker <joshBBparker@gmail.com>
2022-03-15 17:09:39 -0600

    lint password

--- src/components/MyProfile/components/Password.jsx
+++ src/components/MyProfile/components/Password.jsx
@@ -1,62 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Password.scss';
 import axios from 'axios';
 
-class Password extends Component {
-  state = {
-    currentPassword: '',
-    newPassword: '',
-    confirmNewPassword: '',
-  };
+const Password = function Password() {
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmNewPassword, setConfirmNewPassword] = useState('');
 
-  handleChange = (e) => {
-    const key = e.target.name;
-    const { value } = e.target;
-    this.setState({ [key]: value });
-  };
-
-  savePassword = () => {
-    const newPass = this.state.newPassword;
-    const comparePass = this.state.confirmNewPassword;
+  const savePassword = () => {
+    const newPass = newPassword;
+    const comparePass = confirmNewPassword;
     const passwords = {
-      currentPassword: this.state.currentPassword,
-      newPass: this.state.newPassword,
+      currentPassword,
+      newPass: newPassword,
     };
     if (newPass === comparePass) {
       axios.put('/api/auth/password', passwords)
         .then((res) => {
           console.log(res);
-          this.setState({
-            currentPassword: '',
-            newPassword: '',
-            confirmNewPassword: '',
-          });
+          setCurrentPassword('');
+          setNewPassword('');
+          setConfirmNewPassword('');
         });
     } else alert("Passwords don't match");
   };
 
-  render() {
-    return (
-      <div className="personal_info_container">
-        <h2>Change your password</h2>
-        <div>
-          <div className="input_container">
-            <span className="lable">Current Password</span>
-            <input value={this.state.currentPassword} name="currentPassword" type="password" className="current_password" onChange={this.handleChange} />
-          </div>
-          <div className="input_container">
-            <span className="lable">New Password</span>
-            <input value={this.state.newPassword} name="newPassword" type="password" className="new_password" onChange={this.handleChange} />
-          </div>
-          <div className="input_container">
-            <span className="lable">Confirm new Password</span>
-            <input value={this.state.confirmNewPassword} name="confirmNewPassword" type="password" className="confirm_new_password" onChange={this.handleChange} />
-          </div>
+  return (
+    <div className="personal_info_container">
+      <h2>Change your password</h2>
+      <div>
+        <div className="input_container">
+          <span className="lable">Current Password</span>
+          <input value={currentPassword} name="currentPassword" type="password" className="current_password" onChange={({ target }) => setCurrentPassword(target.value)} />
         </div>
-        <button onClick={this.savePassword} className="save-btn">Save</button>
+        <div className="input_container">
+          <span className="lable">New Password</span>
+          <input value={newPassword} name="newPassword" type="password" className="new_password" onChange={({ target }) => setNewPassword(target.value)} />
+        </div>
+        <div className="input_container">
+          <span className="lable">Confirm new Password</span>
+          <input value={confirmNewPassword} name="confirmNewPassword" type="password" className="confirm_new_password" onChange={({ target }) => setConfirmNewPassword(target.value)} />
+        </div>
       </div>
-    );
-  }
-}
+      <button type="submit" onClick={savePassword} className="save-btn">Save</button>
+    </div>
+  );
+};
 
 export default Password;
