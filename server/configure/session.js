82afb3c769ed7ae7bd6ef4f67fe16afba34132ed
Josh Parker <jparker@sherpas.ai>
2022-03-15 12:13:52 -0600

    disallow clear text cookie transmission

--- server/configure/session.js
+++ server/configure/session.js
@@ -2,31 +2,33 @@ const passport = require('passport');
 const session = require('express-session');
 const pgSession = require('connect-pg-simple')(session);
 
+// eslint-disable-next-line new-cap
 const pgStore = new pgSession({
-  conString: process.env.dataBase
+  conString: process.env.dataBase,
 });
 
-module.exports = (app , db) => {
+module.exports = (app, db) => {
   app.use(session({
     store: pgStore,
     secret: 'jasdkfjalksdjflkasdf',
     resave: false,
     saveUninitialized: false,
     cookie: {
-      expires: 5 * 24 * 60 * 60 * 1000 // 5 days
-    }
-  }))
-  app.use(passport.initialize())
-  app.use(passport.session())
+      expires: 5 * 24 * 60 * 60 * 1000, // 5 days
+      secure: true,
+    },
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-  passport.serializeUser((user,done) => {
-    done(null, user[0].id)
-  })
-  passport.deserializeUser((id,done)=> {
+  passport.serializeUser((user, done) => {
+    done(null, user[0].id);
+  });
+  passport.deserializeUser((id, done) => {
     db.user.get_user([id])
-      .then(user => {
+      .then((user) => {
         done(null, user);
       })
-      .catch(done)
-  })
-}
+      .catch(done);
+  });
+};
--- server/routes/board.js
+++ server/routes/board.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const router = express.Router();
 
 const serverError = require('./helpers/server-error');
@@ -9,35 +10,35 @@ const sendBoardID = require('./helpers/send-board-id');
 const onBoard = require('./helpers/on-board');
 
 router.use((req, res, next) => {
-  req.db = req.app.get('db')
+  req.db = req.app.get('db');
   next();
-})
+});
 
 // POST /api/board
 // requires name for new board and id for team it will belong to.
 // user must be manager or approved team member or request will fail.
-router.post('/', isAuthenticated, onTeam, (req, res, next) => {
+router.post('/', isAuthenticated, onTeam, (req, res) => {
   req.db.board.new([req.body.name, req.body.teamID, req.user[0].id])
     .then(() => req.db.board.get_id_by_name([req.body.name]))
-    .then(boards => res.status(200).json(boards[0]))
-    .catch(serverError(res))
-})
+    .then((boards) => res.status(200).json(boards[0]))
+    .catch(serverError(res));
+});
 
 // GET /api/board
 // see the boards you have access to.
-router.get('/', isAuthenticated, (req, res, next) => {
+router.get('/', isAuthenticated, (req, res) => {
   req.db.board.mine([req.user[0].id])
-    .then(boards => res.status(200).json(boards))
+    .then((boards) => res.status(200).json(boards))
     .catch(serverError(res));
-})
+});
 
 // GET /api/board/modifiable
 // see the boards you own or manage
-router.get('/modifiable', isAuthenticated, (req, res, next) => {
+router.get('/modifiable', isAuthenticated, (req, res) => {
   req.db.board.modifiable([req.user[0].id])
-    .then(boards => res.status(200).json(boards))
+    .then((boards) => res.status(200).json(boards))
     .catch(serverError(res));
-})
+});
 
 // GET /api/board/id/:name
 // get board id by name
@@ -46,74 +47,74 @@ router.get('/modifiable', isAuthenticated, (req, res, next) => {
 //       then only the most recently created will be sent
 router.get('/id/:name', isAuthenticated, (req, res, next) => {
   req.db.board.get_id_by_name([req.params.name])
-    .then(id => {
+    .then((id) => {
       if (!id || id.length < 1 || !id[0].id) {
         res.status(404).send('no board located');
         return;
       }
-      sendBoardID(id[0].id, req.params.name, req, res, next)
+      sendBoardID(id[0].id, req.params.name, req, res, next);
     })
-    .catch(serverError(res))
-})
+    .catch(serverError(res));
+});
 
 // GET /api/board/team/:teamID
 // get all the boards belonging to a team
 // you must be an approved team member to access this information
-router.get('/team/:teamID', isAuthenticated, onTeam, (req, res, next) => {
+router.get('/team/:teamID', isAuthenticated, onTeam, (req, res) => {
   req.db.board.on_team([req.params.teamID])
-    .then(boards => res.status(200).json(boards))
+    .then((boards) => res.status(200).json(boards))
     .catch(serverError(res));
-})
+});
 
 // GET /api/board/name/:searchTerm
 // get all the boards you have access to have names beginning with searchTerm
-router.get('/name/:searchTerm', isAuthenticated, (req, res, next) => {
-  req.db.board.name_search([req.params.searchTerm + '%', req.user[0].id])
-    .then(boards => res.status(200).json(boards))
+router.get('/name/:searchTerm', isAuthenticated, (req, res) => {
+  req.db.board.name_search([`${req.params.searchTerm}%`, req.user[0].id])
+    .then((boards) => res.status(200).json(boards))
     .catch(serverError(res));
-})
+});
 
 // GET /api/board/by-id/:boardID
 // get board by id
-router.get('/by-id/:boardID', isAuthenticated, onBoard, (req, res, next) => {
+router.get('/by-id/:boardID', isAuthenticated, onBoard, (req, res) => {
   req.db.board.by_id([req.params.boardID])
-    .then(board => res.status(200).json(board))
+    .then((board) => res.status(200).json(board))
     .catch(serverError(res));
-})
+});
 
 // DELETE /api/board/:boardID
 // must be manager or board owner to delete
-router.delete('/:boardID', isAuthenticated, boardLord, (req, res, next) => {
+router.delete('/:boardID', isAuthenticated, boardLord, (req, res) => {
   req.db.board.delete([req.params.boardID])
     .then(() => res.status(200).send('deleted'))
     .catch(serverError(res));
-})
+});
 
 // PUT /api/board/name
 // request body requires boardID and name
 // must be manager or board owner to rename.
-router.put('/name', isAuthenticated, boardLord, (req, res, next) => {
+router.put('/name', isAuthenticated, boardLord, (req, res) => {
   req.db.board.rename([req.body.name, req.body.boardID])
     .then(() => res.status(200).send('renamed'))
     .catch(serverError(res));
-})
+});
 
 // PUT /api/board/description
 // request body requires boardID and description
 // must be manager or board owner to update description
-router.put('/description', isAuthenticated, boardLord, (req, res, next) => {
+router.put('/description', isAuthenticated, boardLord, (req, res) => {
   req.db.board.description([req.body.description, req.body.boardID])
     .then(() => res.status(200).send('description updated'))
     .catch(serverError(res));
-})
+});
 
 // PUT /api/board/owner
 // request body requires boardID and ownerID
 // must be manager or board owner to transfer ownership
-router.put('/owner', isAuthenticated, boardLord, (req, res, next) => {
+router.put('/owner', isAuthenticated, boardLord, (req, res) => {
   req.db.board.owner([req.body.ownerID, req.body.boardID])
     .then(() => res.status(200).send('ownership transferred'))
     .catch(serverError(res));
-})
+});
 
 module.exports = router;
