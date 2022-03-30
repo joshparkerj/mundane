3c21ce41dde3d6690845b3d61e5816a509e40d82
Josh Parker <joshBBparker@gmail.com>
2022-03-30 16:43:27 -0600

    update deprecated redux devtools dependency

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from '@redux-devtools/extension';
 import reducer from './reducer';
 import history from '../history';
 
